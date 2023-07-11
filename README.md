dda0dbeba72bb2b2c27d0b9241debaa612262ba1
kaline <mesquitabfkaline@gmail.com>
2023-07-10 21:24:48 -0300

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 - https://www.tutorialsteacher.com/d3js/loading-data-from-file-in-d3js
+https://blog.logrocket.com/data-visualization-d3-js-node-js/
--- index.js
+++ index.js
@@ -3,4 +3,20 @@ d3.csv("dados orçamento - 2003-2022 - função, subfunção, resultado primári
         console.log(data[i].Ano);
         console.log(data[i].Função);
     }
+
+    http.createServer(
+        function (req, res) {
+          // favicon - browser annoyance, ignore 
+          if(req.url.indexOf('favicon.ico') >= 0) {
+            res.statusCode = 404
+            return
+          }
+      
+          res.writeHead(200, { "Content-Type": 'image/svg+xml' });
+          res.end(d3Draw());
+        }
+      )
+      .listen(8080, function() {
+        console.log("Server listening on port 8080");
+      });
 });
